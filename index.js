1da184b79bcacb00795b4ec73a7d62030fb6b244
Igor Gissi <gissiigor@gmail.com>
2024-12-03 14:24:51 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let vitorias = 50
+let derrotas = 38
+let saldo = vitorias - derrotas
+saldoRankeadas()
+console.log ("O Herói tem saldo de " + saldo + " está no nível " + saldoRankeadas)
+
+function saldoRankeadas (saldo){
+    if(saldo){
+        saldo < 10
+        saldoRankeadas = "Ferro"
+    }else if (saldo){
+        10 < saldo <= 20
+        saldoRankeadas = "Bronze"
+    }else if (saldo){
+        20 < saldo <= 50
+        saldoRankeadas = "Prata"
+    }else if (saldo){
+        50 < saldo <= 80
+        saldoRankeadas = "Ouro"
+    }else if (saldo){
+        80 < saldo <= 90
+        saldoRankeadas = "Diamante"
+    }else if (saldo){
+        90 < saldo <= 100
+        saldoRankeadas = "Lendário"
+    }else if (saldo){
+        saldo > 100
+        saldoRankeadas = "Imortal"
+    }else {
+        saldoRankeadas = "Ranque não aceito"
+    }
+    return saldoRankeadas
+}
